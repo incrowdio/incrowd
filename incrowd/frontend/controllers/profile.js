d38441cf276b77666c0c1b71584d70653cca7a51
Josh Gachnang <josh@pcsforeducation.com>
2014-12-29 00:24:29 -0800

    Fixing profile editting not working

--- incrowd/frontend/controllers/profile.js
+++ incrowd/frontend/controllers/profile.js
@@ -1,30 +1,29 @@
 angular.module("profile", ["djangle"])
   .controller('ProfileCtrl', ['$scope', '$http', '$routeParams', 'Form', 'BACKEND_SERVER', function ($scope, $http, $routeParams, Form, BACKEND_SERVER) {
     $scope.username = $routeParams.username;
-    console.log('profile ctrl', $scope.username);
     $http.get(BACKEND_SERVER + 'users\/' + $scope.username + '\/')
       .then(function (res) {
-        console.log('profiles', res);
         $scope.profile = res.data;
       });
 
-    $scope.editSave = function(index) {
-      console.log('profiles', $scope.profile, index);
+    $scope.editSave = function (index) {
       $http.put(BACKEND_SERVER + 'users\/' + $scope.profile.username + '\/', $scope.profile);
     }
   }])
+
   .controller('ProfileListCtrl', function ($http, $scope, BACKEND_SERVER) {
-    console.log('profileslistctrl');
     $http.get(BACKEND_SERVER + 'users\/')
       .then(function (res) {
-        console.log('profiles', res);
         $scope.profiles = res.data.results;
       });
 
-    $scope.editSave = function(index) {
-      console.log('profiles', $scope.profiles, index);
+    $scope.editSave = function (index) {
       var profile = $scope.profiles[index];
       $http.put(BACKEND_SERVER + 'users\/' + profile.username + '\/', profile);
+    };
+
+    $scope.log = function (msg) {
+      console.log(msg);
     }
 
   });
