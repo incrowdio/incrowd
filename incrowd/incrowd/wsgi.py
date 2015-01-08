83787e40b2cae597bee37e3a6e9bfb1d282a5098
Josh Gachnang <josh@pcsforeducation.com>
2015-01-07 22:28:03 -0800

    Adding logging to indicate whether running new relic or not

--- incrowd/incrowd/wsgi.py
+++ incrowd/incrowd/wsgi.py
@@ -7,14 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+import logging
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "incrowd.settings")
+logger = logging.getLogger()
 
 try:
     import newrelic.agent
+    logger.info('initializing with new relic')
     newrelic.agent.initialize(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__), 'config/newrelic.ini'))))
 except Exception:
-    pass
+    logger.info('running without newrelic')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
